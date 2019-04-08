87fb96f73872dd10362fb9f6afaa03579d938f58
Michael Evans <mmevansjr@gmail.com>
2019-04-08 15:10:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Hello, Neil deGrasse Tyson"
 
 var greetSpecialGuest = "" + specialGuest + "!";
 
-var topic = 'Let\'s talk about space.';
+var topic = Lets talk about Let's talk about space..;
 
 var conversation = `${topic}`;
